2b7900b7fc105f6012096298a0bd1f670293a3f9
Reese <r3es3y@gmail.com>
2026-06-09 20:56:49 +1200

    2026-06-09 - Attempting Build for gen: 1007

--- core/deploy/src/eval.cpp
+++ core/deploy/src/eval.cpp
@@ -532,8 +532,8 @@ eval::result eval::makeCommandStr(string attrset, string topAttr,
     cerr << utils::error(
         "Mutiple candidates found for attrset (\033[35m" + attrset +
         "\033[0m).\n Please implement\033[94m lambda input parsing\033[0m to "
-        "determine the\033[94m winning candidate\033[0m. \033[35m:3\033[0m\n"
-        "( translation: screw you, sincerely past Reesey )");
+        "determine the\033[94m winning candidate\033[0m. \n"
+        "( translation: screw you, sincerely past Reesey \033[35m:3\033[0m )");
     eval::result res;
     res.error = true;
     return res;
