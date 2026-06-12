7e806db083d5c82864406f2b4b3848f578466c32
Reese <r3es3y@gmail.com>
2026-06-12 20:55:42 +1200

    2026-06-12 - Attempting Build for gen: 1059

--- core/deploy/src/utils.cpp
+++ core/deploy/src/utils.cpp
@@ -17,19 +17,54 @@ using namespace std;
 utils::result utils::runCommand(string cmd) {
   result res;
 
-  FILE *pipe = popen((cmd + " 2>&1").c_str(), "r");
-  if (!pipe) {
+  int stdout_pipe[2], stderr_pipe[2];
+  if (pipe(stdout_pipe) || pipe(stderr_pipe)) {
     res.exitCode = -1;
     res.error = strerror(errno);
     return res;
   }
 
-  char buffer[256];
-  while (fgets(buffer, sizeof(buffer), pipe))
-    res.output += buffer;
+  pid_t pid = fork();
+  if (pid == -1) {
+    res.exitCode = -1;
+    res.error = strerror(errno);
+    return res;
+  }
 
-  int status = pclose(pipe);
-  res.exitCode = (status == -1) ? -1 : WEXITSTATUS(status);
+  if (pid == 0) {
+    // Child: wire up pipes
+    dup2(stdout_pipe[1], STDOUT_FILENO);
+    dup2(stderr_pipe[1], STDERR_FILENO);
+    close(stdout_pipe[0]);
+    close(stdout_pipe[1]);
+    close(stderr_pipe[0]);
+    close(stderr_pipe[1]);
+
+    execl("/bin/sh", "sh", "-c", cmd.c_str(), nullptr);
+    _exit(127);
+  }
+
+  // Parent: close write ends
+  close(stdout_pipe[1]);
+  close(stderr_pipe[1]);
+
+  // Read stdout and stderr
+  char buffer[256];
+  auto readFd = [&](int fd, string &into) {
+    ssize_t n;
+    while ((n = read(fd, buffer, sizeof(buffer))) > 0)
+      into.append(buffer, n);
+  };
+
+  readFd(stdout_pipe[0], res.output);
+  readFd(stderr_pipe[0], res.error);
+
+  close(stdout_pipe[0]);
+  close(stderr_pipe[0]);
+
+  int status;
+  waitpid(pid, &status, 0);
+  res.exitCode = WIFEXITED(status) ? WEXITSTATUS(status) : -1;
   return res;
 }
 string utils::readFile(const string &path) {
