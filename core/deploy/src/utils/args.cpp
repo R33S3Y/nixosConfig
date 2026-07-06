9847c4ca7b1c82a6af00da360630284d8435c5a5
Reese <r3es3y@gmail.com>
2026-07-06 22:05:49 +1200

    2026-07-06 - Attempting Build for gen: 1191

--- core/deploy/src/utils/args.cpp
+++ core/deploy/src/utils/args.cpp
@@ -129,7 +129,7 @@ args::parse(vector<string> userInput, map<string, args::optionIn> argValues) {
     output[argName] = invokedOutput;
   }
 
-  userInput.erase(userInput.end());
+  userInput.erase(userInput.end() - 1);
 
   string remainder = userInput[0];
   for (int i = 1; i < userInput.size(); i++) {
