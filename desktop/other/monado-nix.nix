fcc67e8bad21e71440590dade85c79e28e982777
Reese <r3es3y@gmail.com>
2026-09-15 21:26:16 +1200

    2026-09-15 - Attempting Build for gen: 113

--- desktop/other/monado-nix.nix
+++ desktop/other/monado-nix.nix
@@ -19,5 +19,8 @@
     libsurvive # backend for lighthouse tracking
   ];
 
-  systemd.user.services.monado.environment.STEAMVR_LH_ENABLE = "0";
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "0";
+    LH_HANDTRACKING = "0";
+  };
 }
