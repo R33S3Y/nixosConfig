dd32f7a8b2d29c3396d06990cf3d0ff6b0c756dd
Reese <r3es3y@gmail.com>
2026-09-17 07:42:21 +1200

    2026-09-17 - Attempting Build for gen: 126

--- desktop/vr/index-nix.nix
+++ desktop/vr/index-nix.nix
@@ -22,8 +22,9 @@
 
   systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "0";
-    LH_HANDTRACKING = "0";
+    # LH_HANDTRACKING = "0";
     VK_LOADER_LAYERS_DISABLE = "~all~";
+    XRT_COMPOSITOR_FORCE_WAYLAND = "1";
   };
 
   environment.sessionVariables = {
