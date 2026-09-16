1362cc4f06bfb1b5d802ae3f17dec90b968d4729
Reese <r3es3y@gmail.com>
2026-09-16 19:04:36 +1200

    2026-09-16 - Attempting Build for gen: 118

--- desktop/vr/index-home.nix
+++ desktop/vr/index-home.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }:
 {
   home.file = {
-    "~/.config/openxr/1/active_runtime.json".source =
-      "${pkgs.monado}/share/openxr/1/openxr_monado.json";
-    "~/.local/share/Steam/steamapps/common/SteamVR/steamzr_linux64.json".source =
+    ".config/openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+    ".local/share/Steam/steamapps/common/SteamVR/steamzr_linux64.json".source =
       "${pkgs.monado}/share/openxr/1/openxr_monado.json";
   };
 }
