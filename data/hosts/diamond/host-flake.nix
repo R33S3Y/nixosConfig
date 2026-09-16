2983a474607d3897a01b8735653bf8adbcf29eec
Reese <r3es3y@gmail.com>
2026-09-17 07:05:35 +1200

    2026-09-17 - Attempting Build for gen: 123

--- data/hosts/diamond/host-flake.nix
+++ data/hosts/diamond/host-flake.nix
@@ -51,6 +51,7 @@ let
       # VR
       ../../../desktop/vr/index-nix.nix
       ../../../desktop/vr/svrbsctl-nix.nix
+      ../../../desktop/vr/wayvr-nix.nix
       # VScode
       ../../../desktop/vscode/enable-nix.nix
       # Other
