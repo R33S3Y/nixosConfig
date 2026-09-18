8eab4785d64240cd6e2a9bbe5d345973578df2db
Reese <r3es3y@gmail.com>
2026-09-18 23:38:20 +1200

    2026-09-18 - Attempting Build for gen: 134

--- desktop/vr/index-home.nix
+++ desktop/vr/index-home.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   home.file = {
     ".config/openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
--- desktop/vr/index-nix.nix
+++ desktop/vr/index-nix.nix
@@ -23,6 +23,7 @@
   systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "0";
     # LH_HANDTRACKING = "0";
+    SURVIVE_GLOBALSCENESOLVER = "0";
   };
 
   environment.sessionVariables = {
