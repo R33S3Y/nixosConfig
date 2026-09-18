a401293df839e86509f60411585bdd021ab655e4
Reese <r3es3y@gmail.com>
2026-09-19 10:51:21 +1200

    2026-09-19 - Attempting Build for gen: 136

--- desktop/strawberry/bind-home.nix
+++ desktop/strawberry/bind-home.nix
@@ -7,37 +7,32 @@
         {
           _args = [
             "XF86AudioPrev"
-            lib.generators.mkLuaInline
-            "hl.dsp.exec_cmd(\"strawberry --previous\")"
+            (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"strawberry --previous\")")
           ];
         }
         {
           _args = [
             "XF86AudioPlay"
-            lib.generators.mkLuaInline
-            "hl.dsp.exec_cmd(\"strawberry --play-pause\")"
+            (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"strawberry --play-pause\")")
           ];
         }
         {
           _args = [
             "XF86AudioNext"
-            lib.generators.mkLuaInline
-            "hl.dsp.exec_cmd(\"strawberry --next\")"
+            (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"strawberry --next\")")
           ];
         }
 
         {
           _args = [
             "XF86AudioLowerVolume"
-            lib.generators.mkLuaInline
-            "hl.dsp.exec_cmd(\"strawberry --volume-down\")"
+            (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"strawberry --volume-down\")")
           ];
         }
         {
           _args = [
             "XF86AudioRaiseVolume"
-            lib.generators.mkLuaInline
-            "hl.dsp.exec_cmd(\"strawberry --volume-up\")"
+            (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"strawberry --volume-up\")")
           ];
         }
       ];
