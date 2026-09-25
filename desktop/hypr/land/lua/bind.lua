f3537871a4da528ce02a2b1670c23230e5b62c17
Reese <r3es3y@gmail.com>
2026-09-26 09:40:33 +1200

    2026-09-26 - Attempting Build for gen: 152

--- desktop/hypr/land/lua/bind.lua
+++ desktop/hypr/land/lua/bind.lua
@@ -14,13 +14,6 @@ hl.bind("SUPER + right", hl.dsp.focus({ direction = "right" }))
 hl.bind("SUPER + up", hl.dsp.focus({ direction = "up" }))
 hl.bind("SUPER + down", hl.dsp.focus({ direction = "down" }))
 
--- Switch workspaces with mainMod + [0-9]
--- Move active window to a workspace with mainMod + SHIFT + [0-9]
-for i = 1, 10 do
-  local key = i % 10 -- 10 maps to key 0
-  hl.bind("SUPER + " .. key, hl.dsp.focus({ workspace = i }))
-  hl.bind("SUPER + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
-end
 
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind("SUPER + mouse_down", hl.dsp.focus({ workspace = "r+1" }))
